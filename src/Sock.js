5b86378598bbe475d0de0ee0833c68d691905075
PirumZ <148804044+PirumZ@users.noreply.github.com>
2024-04-24 17:31:34 -0400

    Sock updates

--- src/Sock.js
+++ src/Sock.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 export default function Sock(props) {
   return (
     <div className="sock">
-      
+      <div className="sock-name">
+        {props.brand} {props.name}
+      </div>
+      <div className="fit-mat">
+        {props.fit-type} {props.material}
+      </div>
+      <Button className btn btn-primary p-1 float-end>Add to Cart</Button>
     </div>
   );
 }
