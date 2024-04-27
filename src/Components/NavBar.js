73623e7af7ca40765cd4f69510a6c039cdfa8b2e
PirumZ <128063277+PirumZed@users.noreply.github.com>
2024-04-26 20:57:27 -0400

    I didn't know I could hate NavBars this much

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,18 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function NavigationBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="md" sticky="top">
       <Container>
-        <Navbar.Brand to="/">Just Socks, That's it</Navbar.Brand>
+        <Navbar.Brand>Just Socks, That's it</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/products">Products</Link>
-            <Link to="/cart">Shopping Cart</Link>
-            <Link to="/checkout">Checkout</Link>
+            <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+            <LinkContainer to="/products"><Nav.Link>Products</Nav.Link></LinkContainer>
+            <LinkContainer to="/cart"><Nav.Link>Shopping Cart</Nav.Link></LinkContainer>
+            <LinkContainer to="/checkout"><Nav.Link>Checkout</Nav.Link></LinkContainer>
             </Nav>
         </Navbar.Collapse>
       </Container>
