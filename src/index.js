0c32105f9cfe66847973bd61adbe407fd560a4cb
PirumZ <128063277+PirumZed@users.noreply.github.com>
2024-05-01 18:07:17 -0400

    last time for real

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './CSS/index.css';
 import App from './App';
 
-
+//Page that renders the application, is connected to Index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
